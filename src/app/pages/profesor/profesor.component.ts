8616d0e9acee3baef466477f348ad1fd879d7222
Crisguamann <cristopher.guaman.est@tecazuay.edu.ec>
2023-03-01 08:52:00 -0500

    Componente Profesor

--- src/app/pages/profesor/profesor.component.ts
+++ src/app/pages/profesor/profesor.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Profesor } from './profesor';
+import { ProfesorService } from 'src/app/services/profesor.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-profesor',
@@ -6,5 +10,115 @@ import { Component } from '@angular/core';
   styleUrls: ['./profesor.component.css']
 })
 export class ProfesorComponent {
-  
+  @ViewChild('materiaModalRef') materiaModalRef: any;
+
+  profesores: Profesor[]=[];
+  profesor: Profesor = new Profesor();
+
+
+  constructor(private profesorService: ProfesorService, private router:Router){}
+
+
+
+  ngOnInit(): void {
+    this.profesorService.getAllProfesor().subscribe(
+      profesores => this.profesores = profesores
+    );
+  }
+
+  search(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    const inputValue = inputElement.value;
+    this.profesorService.search(inputValue)
+      .subscribe(response => {
+        this.profesores = response;
+        console.log(response)
+      });
+  }
+
+  get() {
+    this.profesorService.getAllProfesor()
+      .subscribe(response => this.profesor = response);
+
+  }
+
+  editProfesor(profesor: Profesor) {
+    this.profesor.id_profesor = profesor.id_profesor
+    this.profesor.nombre = profesor.nombre
+    this.profesor.segundo_nombre = profesor.segundo_nombre
+    this.profesor.primer_apellido = profesor.primer_apellido
+    this.profesor.segundo_apellido = profesor.segundo_apellido
+    this.profesor.area = profesor.area
+    this.profesor.cedula = profesor.cedula
+    this.profesor.correo = profesor.correo
+    this.profesor.direccion = profesor.direccion
+    this.profesor.fecha_nacimiento = profesor.fecha_nacimiento
+    this.profesor.genero = profesor.genero
+
+    this.materiaModalRef.nativeElement.querySelector('[name="nombre"]').value = profesor.nombre;
+    this.materiaModalRef.nativeElement.querySelector('[name="nombre1"]').value = profesor.segundo_nombre;
+    this.materiaModalRef.nativeElement.querySelector('[name="apellido"]').value = profesor.primer_apellido;
+    this.materiaModalRef.nativeElement.querySelector('[name="apellido1"]').value = profesor.segundo_apellido;
+    this.materiaModalRef.nativeElement.querySelector('[name="direccion"]').value = profesor.direccion;
+    this.materiaModalRef.nativeElement.querySelector('[name="cedula"]').value = profesor.cedula;
+    this.materiaModalRef.nativeElement.querySelector('[name="select"]').value = profesor.genero;
+    this.materiaModalRef.nativeElement.querySelector('[name="fecha"]').value = profesor.fecha_nacimiento;
+    this.materiaModalRef.nativeElement.querySelector('[name="email"]').value = profesor.correo;
+    this.materiaModalRef.nativeElement.querySelector('[name="area"]').value = profesor.area;
+
+  }
+
+  cargarProfesor(): void{
+        this.profesorService.getAllProfesor().subscribe((profesor)=> this.profesor=profesor)
+    
+  }
+
+  public create():void{
+    console.log('se ha hecho un click')
+    this.profesorService.create(this.profesor).subscribe(
+      response => {
+        Swal.fire('Profesor guardado',`Profesor ${response.nombre} guardado con exito`,'success')
+
+      }
+    )
+  }
+
+  update() {
+    this.profesorService.update(this.profesor.id_profesor, this.profesor)
+      .subscribe(response => {
+        this.profesor = new Profesor();
+        this.get();
+        Swal.fire('Profesor modificado',`Profesor ${response.nombre} modificado con exito`,'success');
+      });
+  }
+
+  /*Delete(profesor:Profesor){
+    console.log('Se ha hecho click ene Delete(profesor:Profesor)');
+    this.profesorService.deleteProfesor(profesor)
+    .subscribe(data=>{
+      this.profesores = this.profesores.filter(p=>p!==profesor);
+      Swal.fire('Profesor eliminado',`Profesor ${data.nombre} eliminado con exito`,'error');
+    })
+  }*/
+
+  deleteRecord(id: number) {
+    console.log('Se ha hecho clic en Delete')
+    this.profesorService.deleteMyRecord(id)
+      .subscribe(() => {
+        this.profesores = this.profesores.filter(record => record.id_profesor !== id);
+      });
+  }
+
+  limpiarCampos() {
+    this.profesor.nombre = '';
+    this.profesor.primer_apellido = '';
+    this.profesor.segundo_nombre = '';
+    this.profesor.segundo_apellido = '';
+    this.profesor.direccion = '';
+    this.profesor.cedula = '';
+    this.profesor.genero = '';
+    this.profesor.correo = '';
+    this.profesor.area = '';
+    this.profesor.fecha_nacimiento='';
+  }
 }
